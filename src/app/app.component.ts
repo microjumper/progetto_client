faae3ac4a6b472adaf6b7597244424d7dd9c0f9d
Salvatore Sammarco <microjmpr@gmail.com>
2024-06-04 11:59:57 +0200

    refactoring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import { PrimeNGConfig } from "primeng/api";
+import { MenuItem, PrimeNGConfig } from "primeng/api";
+import { ButtonDirective } from "primeng/button";
+import { MenuModule } from "primeng/menu";
+import { ToolbarModule } from "primeng/toolbar";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+    imports: [RouterOutlet, ButtonDirective, MenuModule, ToolbarModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
 
-  constructor(private primengConfig: PrimeNGConfig) { }
+  items: MenuItem[];
+
+  constructor(private primengConfig: PrimeNGConfig) {
+    this.items = [
+      {
+        label: 'Logout',
+        icon: 'pi pi-sign-out',
+        command: () => console.log('Logout')
+      }
+    ];
+  }
 
   ngOnInit() {
     this.primengConfig.ripple = true;
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,38 +1,27 @@
 import { Component } from '@angular/core';
 import { NgOptimizedImage } from "@angular/common";
+import { RouterLink } from "@angular/router";
 
-import { ToolbarModule } from "primeng/toolbar";
-import { MenuModule } from "primeng/menu";
 import { ButtonDirective } from "primeng/button";
-import { MenuItem } from "primeng/api";
 import { CarouselModule } from "primeng/carousel";
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    ToolbarModule,
-    MenuModule,
     ButtonDirective,
     CarouselModule,
     NgOptimizedImage,
+    RouterLink,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
 
-  items: MenuItem[];
   images: any[];
 
   constructor() {
-    this.items = [
-      {
-        label: 'Logout',
-        icon: 'pi pi-sign-out',
-        command: () => console.log('Logout')
-      }
-    ];
 
     this.images = [
       { source: 'assets/carousel/photo_1.jpg', alt: '' },
